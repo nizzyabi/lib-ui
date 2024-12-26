f9b613c5bf96e9425022dafc506d5feb85e6a3af
NizzyABI <nizabizaher@gmail.com>
2024-12-25 20:36:13 -0500

    posthog

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,21 @@
-'use client'
+"use client";
 
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
+    person_profiles: "identified_only",
+  });
+}
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <>
+      <SessionProvider>
+        <PostHogProvider client={posthog}>{children}</PostHogProvider>
+      </SessionProvider>
+    </>
+  );
 }
